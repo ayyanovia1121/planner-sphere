76815b4c7f6aa3dc9e7c6a68e12dcb673c6204a9
ayyanovia1121 <noviayya1121@gmail.com>
2025-01-04 14:26:17 +0700

    Shadcn Intallation

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,9 +1,18 @@
 const Header = () => {
   return (
-    <div className="">
-        <h1 className="text-home font-semibold text-[56px]">Header</h1>
-    </div>
-  )
+    <header className="gap-4">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <h1 className="text-[#878EB2] font-extrabold text-[56px]">2025</h1>
+        <h2 className="text-[#878EB2] font-bold text-[20px]">
+          This planner belongs to:
+        </h2>
+        <h2 className="text-[#878EB2] font-bold text-[20px]">
+          Ayyana Noviyarti
+        </h2>
+      </div>
+      <div className="border-b-[3px] w-[400px] border-[#878EB2] h-[23px]"></div>
+    </header>
+  );
 }
 
 export default Header
